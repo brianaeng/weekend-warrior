840f5fe90309292d65c46f2def44860cb32cec8e
Briana Eng <brianaeng@gmail.com>
2016-09-11 21:15:12 -0700

    New solution(s) added. Added explanatory comments.

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,20 +1,26 @@
 class Hamming
 
+  #Class method to compare two strings for Hamming distance
   def self.compute(dna1, dna2)
+
+    #Checks if the strings are the same length, raises error if different
     if dna1.length != dna2.length
       raise ArgumentError
+
+    #Calculates the Hamming difference
     else
+      #Turns strings to arrays
       dna1_array = dna1.split("")
       dna2_array = dna2.split("")
 
+      #Initializes Hamming difference
       ham_diff = 0
 
-      counter = 0
-      dna1_array.each do |letter|
-        if letter != dna2_array[counter]
+      #Compares both arrays by shifting off next item of each, adds to ham_diff if different
+      dna1.length.times do
+        if dna1_array.shift != dna2_array.shift
           ham_diff += 1
         end
-        counter += 1
       end
 
       return ham_diff
@@ -22,3 +28,32 @@ class Hamming
   end
 
 end
+
+# # Initial solution:
+# dna1_array = dna1.split("")
+# dna2_array = dna2.split("")
+#
+# ham_diff = 0
+#
+# counter = 0
+# dna1_array.each do |letter|
+#   if letter != dna2_array[counter]
+#     ham_diff += 1
+#   end
+#   counter += 1
+# end
+#
+# return ham_diff
+
+# # Another solution:
+# array = (dna1.split("")).zip(dna2.split(""))
+#
+# ham_diff = 0
+#
+# array.each do |item|
+#   if item[0] != item[1]
+#     ham_diff += 1
+#   end
+# end
+#
+# return ham_diff
